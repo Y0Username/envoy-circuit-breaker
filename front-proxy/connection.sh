03ad05c548d04e416b9210ea201825faa88ef0f2
Yathish Gangolli <yathig@amazon.com>
2018-12-21 13:18:43 -0800

    Adding missed files

---
+++ front-proxy/connection.sh
@@ -0,0 +1,1 @@
+curl -i -H "Content-Type: json" -H "Content-Length: 8000" -X POST -d '{}' http://localhost:8080/service/$1
---
+++ front-proxy/post.sh
@@ -0,0 +1,1 @@
+curl -i -H "Content-Type: json" -X POST -d '{"envoy":"hi"}' http://localhost:8080/service/$1
